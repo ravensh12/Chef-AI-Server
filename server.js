b2654d9c27ae223545f793cf926e16700b806e9f
Shravan Venkat <raven@Mac.attlocal.net>
2025-08-01 01:10:10 -0500

    Trigger: force Render redeploy

--- server.js
+++ server.js
@@ -69,3 +69,4 @@ app.post('/api/generate-recipe', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+// Temporary comment to trigger deploy
